7f78cd1588e18dd47ecb7118fef52380ffb9df17
Anurag-git04 <shawanurag18@gmail.com>
2024-03-18 23:53:16 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+require("dotenv").config();
+
+const dbConnect = () => {
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology: true,
+    })
+    .then(()=> console.log("DB ka connection succesfull"))
+    .catch((error) => {
+        console.log("Issue in DB connection")
+        console.error(error.message)
+        //kya hai yeh
+        process.exit(1);
+    })
+}
+
+module.exports = dbConnect; 
