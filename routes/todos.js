b2e173db01b92421d4ddfbc7771462c4f21fbe4f
Anurag-git04 <shawanurag18@gmail.com>
2024-03-29 21:59:04 +0530

    ""

--- routes/todos.js
+++ routes/todos.js
@@ -5,10 +5,12 @@ const router = express.Router();
 //import controller
 const {createTodo} = require("../controllers/createTodo");
 const {getTodo,getTodoById} = require("../controllers/getTodo");
+const {updateTodo} = require("../controllers/updateTodo");
 
 //define API routes
 router.post("/createTodo", createTodo);
 router.get("/getTodo",getTodo)
 router.get("/getTodo/:id",getTodoById)
+router.put("/updateRodo/:id",updateTodo)
 
 module.exports = router;
